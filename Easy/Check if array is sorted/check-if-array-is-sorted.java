9ebd532ccc67fe8abac1304dc7a879e003e5703e
AgarwalMaddy <madhur1812agarwal@gmail.com>
2023-07-12 00:02:50 +0530

    Questions

---
+++ Easy/Check if array is sorted/check-if-array-is-sorted.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+  public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            boolean ans = new Solution().arraySortedOrNot(arr, n);
+            System.out.println(ans ? 1 : 0);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean arraySortedOrNot(int[] arr, int n) {
+        // code here
+        int min = arr[0];
+        int i = 1;
+        while(i < n){
+            if(arr[i] >= min){
+                min = arr[i];
+                i++;
+            } else {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
